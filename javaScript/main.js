ce04cc18f12daaad22e7a5605acd8b83fdf13668
Johan <johanomwikstrom@gmail.com>
2019-10-25 10:43:58 +0200

    Added button start quiz for AboutMalmö

---
+++ javaScript/main.js
@@ -0,0 +1,33 @@
+// Below contains quizes
+class Quiz{
+_createQuestion  = "";
+_createAnswer    = "";
+_madeQuestion    = "";
+_madeAnswer    = "";
+
+constructor(){
+    this._createQuestion = createQuestion;
+    this._createAnswer = createAnswer;
+}
+
+}
+
+// Above contains quizes
+
+
+
+
+// Below contains functions for buttons
+function showAboutMalmoQuiz(){
+    let malmoQuestions ;
+  if (malmoQuestions.style.display === "none") {
+    malmoQuestions.style.display = "block";
+  } else {
+    malmoQuestions.style.display = "none";
+  }
+}
+// Above contains functions for buttons
+
+class AboutMalmoQuizClass{
+    
+}
